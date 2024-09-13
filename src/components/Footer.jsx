df8f75e4048d55ccc5733219afccc84e8a09c682
Valentina1012 <valentiki15@gmail.com>
2024-09-13 12:32:56 -0300

    Se agregaron estilos al footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 /* eslint-disable react/prop-types */
+import '../styles/footer.css'
+
 export function Footer() {
     return (
-        <div>
+        <div className="footer-description">
             <p>Creado por mi :)</p>
             <a href="https://github.com/Valentina1012">Github</a>
         </div>
